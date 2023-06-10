57750a8a73b082a27c31c4c27798c638dcdf7c0e
DanM <dan36@ya.ru>
2023-06-10 16:11:14 +0300

    made 0 and 1 tasks

---
+++ task0/Program.cs
@@ -0,0 +1,6 @@
+﻿// writin app making squares from numbers
+Console.Write("Write a number here: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"The square of number {number} is {square}");
+
---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+Console.Write("Enter first (square) number :");
+int square=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second (simple) number :");
+int number=Convert.ToInt32(Console.ReadLine());
+
+if(square==number*number)
+{
+    Console.WriteLine($"{square} is the square of {number}");
+}
+else
+{
+    Console.WriteLine($"{square} is NOT the square of {number}");
+}
