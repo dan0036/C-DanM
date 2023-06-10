0ea3707057f3e42ba1c2049378d82c6cad1f780d
DanM <dan36@ya.ru>
2023-06-10 17:19:36 +0300

    my version of task3

---
+++ task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Week days per numbers
+Console.Write("Enter a number of a day of a week here: ");
+int numerOfADay=Convert.ToInt32(Console.ReadLine());
+if(numerOfADay==1)
+{
+    Console.WriteLine("Today is Monday");
+} 
+else if(numerOfADay==2)
+{
+    Console.WriteLine("Today is Tuesday");
+} 
+else if(numerOfADay==3)
+{
+    Console.WriteLine("Today is Wednesday");
+} 
+else if(numerOfADay==4)
+{
+    Console.WriteLine("Today is Thursday");
+} 
+else if(numerOfADay==5)
+{
+    Console.WriteLine("Today is Friday");
+} 
+else if(numerOfADay==6)
+{
+    Console.WriteLine("Today is Saturday");
+} 
+else if(numerOfADay==7)
+{
+    Console.WriteLine("Today is Sunday");
+} 
+else 
+Console.WriteLine("Wrong number input");
