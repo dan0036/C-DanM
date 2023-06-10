e5b4b2d2cac6d357aa808e9fcf8787c78efe7aac
DanM <dan36@ya.ru>
2023-06-10 23:02:00 +0300

    new changes in 5 n 7

--- task5/Program.cs
+++ task5/Program.cs
@@ -2,17 +2,17 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 // 2 -> " -2, -1, 0, 1, 2"
 System.Console.Write("Input a positive number: ");
-int number=Convert.ToInt32(Console.ReadLine());
-if (number<0)
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
 {
     System.Console.WriteLine("The number is negative!");
 }
 else
 {
-int number2=0; int i=-number; 
-while (number2<number*2+1)
-{
-     System.Console.Write($"{i}, ");
-     i++; number2++;
-}
+    int number2 = 0; int i = -number;
+    while (number2 < number * 2 + 1)
+    {
+        System.Console.Write($"{i}, ");
+        i++; number2++;
+    }
 }
--- task7/Program.cs
+++ task7/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 // 456 -> 6
 System.Console.Write("Input a positive 3-digit number: ");
-int number=Convert.ToInt32(Console.ReadLine());
-if (999<number||number<100)
+int number = Convert.ToInt32(Console.ReadLine());
+if (999 < number || number < 100)
 {
     System.Console.WriteLine("The number is not three-digital!");
 }
 else
 {
-int a=number%10;
-System.Console.WriteLine(a);
+    int a = number % 10;
+    System.Console.WriteLine(a);
 }
