96ab6f109b016383c7ae442715e0cf57478be7b9
DanM <dan36@ya.ru>
2023-06-11 12:41:58 +0300

    task8 h/w done

---
+++ task8_HomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+System.Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 2)
+{
+    System.Console.WriteLine("Please, input number >1.");
+}
+else
+{
+    if (1 == number % 2)
+    { number -= 1; }
+            int i = 2;
+        while (i <= number)
+        {
+            System.Console.Write(i + ", ");
+            i += 2;
+        }
+}
