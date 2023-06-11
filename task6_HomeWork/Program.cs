993784b78198dd98a8fb9be64c21abd249b3a6a0
DanM <dan36@ya.ru>
2023-06-11 11:34:58 +0300

    task6 h/w done

---
+++ task6_HomeWork/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка)
+System.Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (0==number%2)
+{
+    System.Console.WriteLine($"The number {number} is even");
+}
+else if (1==Math.Abs(number%2))
+{
+    System.Console.WriteLine($"The number {number} is odd");
+}
+else
+{
+    System.Console.WriteLine("Ooops, something went wrong(");
+}
