e484e7d5942ab0a9335e5f4044b5e3aa723e45cf
DanM <dan36@ya.ru>
2023-06-14 00:44:04 +0300

    task15

---
+++ task15_HomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] A = {"no", "no","no","no","no","yes","yes",};
+System.Console.WriteLine(A[num-1]);
